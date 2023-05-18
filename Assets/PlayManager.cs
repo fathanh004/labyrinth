cb6eb598c3df5178a87edb84bb741965c73af580
Fathan Hidayatullah <fathanhidayatullah.2020@student.uny.ac.id>
2023-05-19 03:20:03 +0700

    first commit

---
+++ Assets/PlayManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    [SerializeField] GameObject finishedCanvas;
+    [SerializeField] TMP_Text finishedText;
+    int coin = 100; //TODO
+    public void GameOver()
+    {
+        finishedText.text = "Game Over";
+        finishedCanvas.SetActive(true);
+    }
+
+    public void PlayerWin()
+    {
+        finishedText.text = "You Win\nScore: " + GetScore();
+        finishedCanvas.SetActive(true);
+    }
+
+    private int GetScore()
+    {
+        return coin * 10;
+    }
+}
